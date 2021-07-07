eea9111fbe44bbf97877a0b405ffa16c7da726db
= <daniel.insuasti@correounivalle.edu.co>
2021-07-07 18:24:58 -0500

    Terminar Clase nota, Utilizacion de App.java para probar clase nota

--- GRUPO 21/clase04maven/src/main/java/co/edu/utp/misiontic2022/c2/Nota.java
+++ GRUPO 21/clase04maven/src/main/java/co/edu/utp/misiontic2022/c2/Nota.java
@@ -6,8 +6,10 @@ public class Nota {
     private double escala5;
     private String cualitativa;
     private String nombre; //Evaluación, taller, o reto correspondiente
-    private static final int NUMERO_NOTAS = 5;
+    //private static final int NUMERO_NOTAS = 5;
 
+
+    //2)Constructores o Metodos constructores:
     Nota(){
         this.escala100 = 0;
         this.escala5 =0;
@@ -40,7 +42,45 @@ public class Nota {
         this.cualitativa = pCualitativo;
     }
 
+    //3)  Metodos -> Comportamiento que quiero que tenga la clase
+    //Forma especifica de mostrar información
+    
+    public void mostrarNotasConsola(){
+        System.out.println("----------InfoNota--------");
+        System.out.println("Valor Escala 5 -> " + this.escala5);
+        System.out.println("Valor Escala 100 -> " +this.escala100);
+        System.out.println("Valor Escala Cualitativa -> " +this.cualitativa);
+    }
+
+
+    //4)  Getters
+    public String getCualitativa(){
+        return cualitativa;
+    }
+
+    public int getEscala100(){
+        return escala100;
+    }
+
+    public double getEscala5(){
+        return escala5;
+    }
+
+    //5) Setters
+
+    public void setCualitativa(String nuevaCualitativa){
+        this.cualitativa = nuevaCualitativa;
+    }
+
+    public void setEscala100(int nuevaEscala100){
+        this.escala100 = nuevaEscala100;
+    }
+
+    public void setEscala5(double nuevaEscala5){
+        this.escala5 = nuevaEscala5;
+    }
 
     
+
     
 }
