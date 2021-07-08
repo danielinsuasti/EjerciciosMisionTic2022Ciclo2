a4edb8e99be6aa931f0b24e62caa450467da0606
= <daniel.insuasti@correounivalle.edu.co>
2021-07-08 18:55:56 -0500

    Inicio Ejercicio Ajedrez con Clase

---
+++ GRUPO 51/clase04ajedrez/src/main/java/co/edu/utp/misiontic2022/c2/Casilla.java
@@ -0,0 +1,15 @@
+package co.edu.utp.misiontic2022.c2;
+
+
+public class Casilla 
+{
+    private String color;
+    private Integer fila;
+    private Integer columna;
+
+
+    public Boolean Ocupada(){
+        return true;
+    }
+    
+}
---
+++ GRUPO 51/clase04ajedrez/src/main/java/co/edu/utp/misiontic2022/c2/Tablero.java
@@ -0,0 +1,18 @@
+package co.edu.utp.misiontic2022.c2;
+
+
+public class Tablero 
+{
+    public Boolean HacerEnroque(){
+        return true;
+    }
+
+    public Boolean HacerJaque(){
+        return true;
+    }
+
+    public Boolean HacerJaqueMate(){
+        return true;
+    }
+    
+}
