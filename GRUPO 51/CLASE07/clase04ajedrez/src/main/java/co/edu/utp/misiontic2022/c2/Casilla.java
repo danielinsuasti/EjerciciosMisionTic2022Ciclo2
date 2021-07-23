93fa67d5fa8ac3f98a0d578b556f58c649bdd505
= <daniel.insuasti@correounivalle.edu.co>
2021-07-22 19:04:05 -0500

    implementacion relacion de composicion tablero y casilla, caso Ajedrez

--- GRUPO 51/CLASE07/clase04ajedrez/src/main/java/co/edu/utp/misiontic2022/c2/Casilla.java
+++ GRUPO 51/CLASE07/clase04ajedrez/src/main/java/co/edu/utp/misiontic2022/c2/Casilla.java
@@ -3,11 +3,20 @@ package co.edu.utp.misiontic2022.c2;
 
 public class Casilla implements Dibujable
 {
+    //atributos
     private Color color;//NEGRO, negro, NEGro, blanco, BLANCO
     private Integer fila;
     private Integer columna;
 
+    //constructor
+    Casilla(Integer fila, Integer columna){
+        this.fila = fila;
+        this.columna = columna;
 
+        this.color = (fila + columna) % 2 ==0 ? color.BLANCO : color.NEGRO;
+
+    }
+    
     public Boolean Ocupada(){
         return true;
     }
--- GRUPO 51/CLASE07/clase04ajedrez/src/main/java/co/edu/utp/misiontic2022/c2/Tablero.java
+++ GRUPO 51/CLASE07/clase04ajedrez/src/main/java/co/edu/utp/misiontic2022/c2/Tablero.java
@@ -3,6 +3,22 @@ package co.edu.utp.misiontic2022.c2;
 
 public class Tablero
 {
+    //Atributos
+    private Casilla[] casillas;// = new Casilla[64];
+
+    //Constructor
+    public Tablero(){
+        casillas = new Casilla[64];
+        
+        for(int i = 0; i < 64; i++){
+            casillas[i] = new Casilla(i/8, i%8);
+        }
+
+    }
+
+
+
+
     public Boolean HacerEnroque(){
         return true;
     }
