eb42b62ce57f81d81c48f150e15efd20f43db315
= <daniel.insuasti@correounivalle.edu.co>
2021-08-03 20:46:16 -0500

    Ejemplo Clase, jdbc Basico

---
+++ GRUPO 51/CLASE14/jdbc_basico/src/main/java/co/edu/utp/misiontic2022/c2/App.java
@@ -0,0 +1,54 @@
+package co.edu.utp.misiontic2022.c2;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main(String[] args) {
+        String url = "jdbc:sqlite:hr.db";
+        Connection conn = null;
+        Statement stmt = null;
+        ResultSet rs= null;
+
+        try {
+            conn = DriverManager.getConnection(url);
+            stmt = conn.createStatement();
+            rs = stmt.executeQuery("SELECT * FROM jobs");
+
+            while(rs.next()){
+                int id_job = rs.getInt("job_id");
+                String title_job = rs.getString("job_title");
+                int salario_min = rs.getInt("min_salary");
+                int salario_max = rs.getInt("max_salary");
+                //Hacer lo que quiera con los datos leidos
+                System.out.println( "Id_Trabajo= " + id_job + 
+                                    " Nombre Trabajo = "+ title_job + 
+                                    " Salario Maximo = "+ salario_max);
+
+            }
+
+        } catch (SQLException e) {
+            // Manejo de los errores
+        }finally {
+            try {
+            if (rs != null){
+            rs.close();
+            }
+            if (stmt != null) {
+            stmt.close();
+            }
+            if (conn != null){
+            conn.close();
+            }
+            } catch (SQLException e) {
+            }
+    }
+}
+}
